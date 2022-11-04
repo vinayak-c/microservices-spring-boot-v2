948cce8edaff13da38b4c06504dfb126a7d13dcb
Vinayak <vinayakchaurasia99@gmail.com>
2022-11-04 21:44:11 +0530

    Migrated Inventory service to multi module project

---
+++ inventory-service/src/main/java/com/vinayak/microservice/v2/inventoryservice/repository/InventoryRepository.java
@@ -0,0 +1,12 @@
+package com.vinayak.microservice.v2.inventoryservice.repository;
+
+import com.vinayak.microservice.v2.inventoryservice.model.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+    Optional<Inventory> findBySkuCode(String skuCode);
+}
---
+++ inventory-service/src/main/java/com/vinayak/microservice/v2/inventoryservice/service/InventoryService.java
@@ -0,0 +1,19 @@
+package com.vinayak.microservice.v2.inventoryservice.service;
+
+import com.vinayak.microservice.v2.inventoryservice.repository.InventoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class InventoryService {
+
+    private final InventoryRepository inventoryRepository;
+
+    @Transactional(readOnly = true)
+    public boolean isInStock(String skuCode){
+        return inventoryRepository.findBySkuCode(skuCode).isPresent();
+    }
+
+}
