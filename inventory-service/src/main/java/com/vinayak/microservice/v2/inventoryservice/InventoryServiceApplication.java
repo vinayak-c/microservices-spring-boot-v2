8e18f1ef59bc43dfdbfa9336790ff53a8f8570ce
Vinayak <vinayakchaurasia99@gmail.com>
2022-11-06 22:19:12 +0530

    Updated products for testing

--- inventory-service/src/main/java/com/vinayak/microservice/v2/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/vinayak/microservice/v2/inventoryservice/InventoryServiceApplication.java
@@ -23,7 +23,7 @@ public class InventoryServiceApplication {
 
 			Inventory inventory1 = new Inventory();
 			inventory1.setSkuCode("iphone_13_pro");
-			inventory1.setQuantity(5);
+			inventory1.setQuantity(0);
 
 			inventoryRepository.save(inventory);
 			inventoryRepository.save(inventory1);
