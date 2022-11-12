fd955dd8ab2151abae6fff6e12f497f8cbb93c3e
Vinayak <vinayakchaurasia99@gmail.com>
2022-11-12 19:54:55 +0530

    Updated inventory service url config for eureka orderservice instance

--- order-service/src/main/java/com/vinayak/microservice/v2/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/vinayak/microservice/v2/orderservice/service/OrderService.java
@@ -24,7 +24,7 @@ import java.util.stream.Stream;
 public class OrderService {
 
     private final OrderRepository orderRepository;
-    private final WebClient webClient;
+    private final WebClient.Builder webClientBuilder;
 
     public void placeOrder(OrderRequest orderRequest){
         Order order = new Order();
@@ -40,8 +40,8 @@ public class OrderService {
         List<String> skuCodes = order.getOrderLineItemsList().stream().map(orderLineItems1 -> orderLineItems1.getSkuCode()).collect(Collectors.toList());
 
         //call inventory service and allow order if product in stock
-        InventoryResponse[] inventoryResponses = webClient.get()
-                .uri("http://localhost:8082/api/inventory",
+        InventoryResponse[] inventoryResponses = webClientBuilder.build().get()
+                .uri("http://inventory-service/api/inventory",
                         uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
                 .retrieve()
                 .bodyToMono(InventoryResponse[].class)
