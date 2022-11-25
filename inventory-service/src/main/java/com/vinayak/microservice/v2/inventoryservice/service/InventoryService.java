b922117a2801c637bc0e9d228181beef9a0885d9
Vinayak <vinayakchaurasia99@gmail.com>
2022-11-25 21:47:14 +0530

    Added simulation for resilience testing

--- inventory-service/src/main/java/com/vinayak/microservice/v2/inventoryservice/service/InventoryService.java
+++ inventory-service/src/main/java/com/vinayak/microservice/v2/inventoryservice/service/InventoryService.java
@@ -3,6 +3,8 @@ package com.vinayak.microservice.v2.inventoryservice.service;
 import com.vinayak.microservice.v2.inventoryservice.model.InventoryResponse;
 import com.vinayak.microservice.v2.inventoryservice.repository.InventoryRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,12 +13,17 @@ import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class InventoryService {
 
     private final InventoryRepository inventoryRepository;
 
     @Transactional(readOnly = true)
+    @SneakyThrows
     public List<InventoryResponse> isInStock(List<String> skuCode) {
+        log.info("Wait started for slow response simulation");
+        Thread.sleep(10000);
+        log.info("Wait ended for slow response simulation");
         return inventoryRepository.findBySkuCodeIn(skuCode).stream()
                 .map(inventoryResponse ->
                         InventoryResponse.builder()
